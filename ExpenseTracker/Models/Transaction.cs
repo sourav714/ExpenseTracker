17c08fcd79cf9f8d8f3d5ed87bdb4b659d7d5f9f
LAPTOP-0F7DP1SC\HP <singhsourav0215@gmail.com>
2022-12-19 10:35:50 +0530

    Initial commit for the app

---
+++ ExpenseTracker/Models/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Models
+{
+	public class Transaction
+	{
+		[Key]
+		public int TransactionId { get; set; }
+
+
+		public int CategoryId { get; set; }
+
+		public int Amount { get; set; }
+
+		[Column(TypeName = "nvarchar(50)")]
+		public string? Note { get; set; }
+
+		public DateTime Date { get; set; } = DateTime.Now;
+
+		public Category? Category { get; set; }
+
+	}
+}
